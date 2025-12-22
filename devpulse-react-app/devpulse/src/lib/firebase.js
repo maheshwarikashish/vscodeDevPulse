62af88583d517be1e25695b68d53aa592d74fa37
maheshwarikashish411 <maheshwarikashish411@gmail.com>
2025-12-22 06:34:17 +0000

    apikey error resolve

--- devpulse-react-app/devpulse/src/lib/firebase.js
+++ devpulse-react-app/devpulse/src/lib/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // PASTE YOUR CONFIG OBJECT HERE
 const firebaseConfig = {
-  apiKey: "AIzaSyDgaCFid4IaE3sP2BUe4iTVZFi1LpijkeA",
-  authDomain: "devpulse-735de.firebaseapp.com",
-  projectId: "devpulse-735de",
-  storageBucket: "devpulse-735de.firebasestorage.app",
-  messagingSenderId: "1004389043770",
-  appId: "1:1004389043770:web:2b0481ec9a1e7c46122f03"
+  apiKey: "AIzaSyAFwmNxzZKvZiKL9GkP0unqrL8vjOR_6Rw",
+  authDomain: "code-45577.firebaseapp.com",
+  projectId: "code-45577",
+  storageBucket: "code-45577.firebasestorage.app",
+  messagingSenderId: "212800166176",
+  appId: "1:212800166176:web:04a40b82ae1eb0b7fe1f45"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
